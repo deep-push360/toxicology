077508f5d5ee07f130e2baf593f88066e4ae5f2c
sammens19 <sammens19@gmail.com>
2018-01-22 09:09:10 +0200

    'changes'

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
 """Contributors: Samuel Mensah"""
 
 def add(a,b):
 	return a+b
-=======
-"""Contributors: """
+
 def multiply(a,b):
 	return (a*b) 
->>>>>>> 61eafbb0ba50bd6299ca181195c641936ea933d5
+
