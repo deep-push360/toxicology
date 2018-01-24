646a02a66c770df5e5226f9ae8a06cbfbc13f249
zhamel <zhamel92@gmail.com>
2018-01-24 16:06:13 +0200

    update preprocessing function

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,14 +1,18 @@
-"""Contributors: Samuel Mensah"""
+"""Contributors: Zoe Hamel, Samuel Mensah"""
 
 def data_generator(dataset):
     """
-    a function to return x and y
-    where
-    x = list of comments
-    y = a one hot vector of the classes
-    """
-    x = np.array(df['comment_text'])
-    x.tolist()
-    y = np.array(dataset.iloc[:,2:])
-    return(x,y)
+   a function to return x and y
+   where
+   x = list of comments
+   y = a one hot vector of the classes
+   """
 
+    # remove punctuation
+    dataset['comment_text'] = [re.sub('[^\w\s]|(\n)',' ', i) for i in dataset['comment_text']]
+
+    # create list and array
+    x = list(dataset['comment_text'])
+    y = np.array(dataset.iloc[:,2:])
+    
+    return[x,y]
