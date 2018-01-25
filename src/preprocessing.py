0357fa0cd9287350be6283f3569ea940f49ad045
Samuel Ofosu Mensah <samuel@debmower_adj>
2018-01-25 19:50:36 +0200

    modified function to read csv

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,8 +4,11 @@ def clean_punctuation(dataset):
    """
    a function that removes all punctuation
    this function takes in dataset as argument.
-   where dataset is the whole data, it will be a matrix
+   where dataset is a csv file
+
+   library needed for this function is pandas.
    """
+   dataset = pd.read_csv(dataset)
    
    # remove punctuation
    # a list comprehension to remove punctuations
