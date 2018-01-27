cbf491d601622c606d7cda1d42bbdab5741c39c0
kayodeolaleye <kaykola.olaleye@gmail.com>
2018-01-27 23:57:46 +0200

    moved files

---
+++ src_tf/model_rnn.py
@@ -0,0 +1,13 @@
+"""Contributors: Kayode"""
+
+def add(a,b):
+	return a+b
+
+
+def sum(a,z):
+	"""This function computes the sum of terms from a to z"""
+	sum = 0
+	for i in range(a,z+1):
+		sum = sum + i
+	return sum
+
